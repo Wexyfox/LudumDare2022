44bd9bbe958f5b97f6172b5d747e60d684aae46f
Wexyfox <wexyfox@gmail.com>
2022-04-04 20:17:59 +0100

    Wave spawner and game end points completed
    Game ends when player loses all lives or when there are no more waves to be spawned.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public void GameWin()
+    {
+        Debug.Log("Game won");
+    }
+
+    public void GameLoss()
+    {
+        Debug.Log("Game loss");
+    }
+}
